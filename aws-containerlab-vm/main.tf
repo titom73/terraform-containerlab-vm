b56cbab287c5f14333cde8a66c248875856f311b
Thomas Grimonet <tgrimonet@arista.com>
2022-06-22 15:29:54 +0200

    Feat: Use full inline provisioner for docker

--- aws-containerlab-vm/main.tf
+++ aws-containerlab-vm/main.tf
@@ -24,9 +24,14 @@ resource "aws_instance" "web1" {
 
     provisioner "remote-exec" {
         inline = [
-            "sudo bash -c \"$(curl http://www.packetanglers.com/installdocker.sh)\"",
-            "sudo bash -c \"$(curl -sL https://get-clab.srlinux.dev)\"",
+            "sudo apt update",
+            "sudo apt install apt-transport-https ca-certificates curl software-properties-common -y",
+            "curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -",
+            "sudo add-apt-repository \"deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable\"",
+            "sudo apt-cache policy docker-ce",
+            "sudo apt install docker-ce -y",
             "sudo usermod -aG docker ubuntu",
+            "sudo bash -c \"$(curl -sL https://get-clab.srlinux.dev)\"",
             "sudo apt-get update && sudo apt-get install -y git"
         ]
     }
