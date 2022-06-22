673a64bd39721415189b27bf8f1504257b00c0fd
Thomas Grimonet <tgrimonet@arista.com>
2022-06-22 15:05:33 +0200

    Fix: Use public_subnet var instead of hardcoding value

--- aws-containerlab-vm/network.tf
+++ aws-containerlab-vm/network.tf
@@ -12,7 +12,7 @@ resource "aws_vpc" "prod-vpc" {
 
 resource "aws_subnet" "prod-subnet-public-1" {
     vpc_id = "${aws_vpc.prod-vpc.id}"
-    cidr_block = "10.0.1.0/24"
+    cidr_block = "${var.public_subnet}"
     map_public_ip_on_launch = "true" //it makes this a public subnet
     availability_zone = "${var.availability_zone}"
 
