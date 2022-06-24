87f5c51e255f6ea783a274f52fb0c82ce9b49a0b
Thomas Grimonet <tgrimonet@arista.com>
2022-06-24 11:01:59 +0200

    Feat: Move provisioning script outside of each module

--- scripts/provision-ubuntu.sh
+++ scripts/provision-ubuntu.sh
@@ -1,11 +1,28 @@
 #!/bin/sh
 
+# Basic tooling
 sudo apt update
 sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+
+# Patch PATH in bash
+export PATH="${PATH}:/home/ubuntu/.local/bin" >> ~/.bashrc
+
+# Install docker
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 sudo apt-cache policy docker-ce
 sudo apt install docker-ce -y
 sudo usermod -aG docker ubuntu
+
+# Install Containerlab
 sudo bash -c "$(curl -sL https://get-clab.srlinux.dev)"
-sudo apt-get update && sudo apt-get install -y git
+
+# Fix for large topology in Containerlab for cEOS
+sudo sh -c 'echo "fs.inotify.max_user_instances = 50000" > /etc/sysctl.d/99-zceos.conf'
+sudo sysctl -w fs.inotify.max_user_instances=50000
+
+# Basic tooling
+sudo apt install  -y git make zsh python3-pip
+
+# Python packages
+pip install eos-downloader
